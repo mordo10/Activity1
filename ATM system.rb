c14fee17ec003d2e9e4d02dd0de2f26ad4255aaf
mordo10 <50899293+mordo10@users.noreply.github.com>
2019-05-23 14:02:09 +0800

    Add files via upload

---
+++ ATM system.rb
@@ -0,0 +1,12 @@
+puts "Welcome!"
+puts "Please Register "
+print "Name: "
+ var1 = gets
+print "Phone Number: "
+  var2 = gets
+
+puts "Menu: "
+ my_hash = {"1" => "Deposit","2" => "Withdraw","3" => "Check", "4" => "Exit"}
+  my_hash.each do ['key','value']
+    puts my_hash
+    end
